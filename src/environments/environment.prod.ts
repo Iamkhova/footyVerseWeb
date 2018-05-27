5ae6c15d7c0b3fc380014edf7fb664ce64b84673
Kareem Glover <iamkhova@gmail.com>
2018-05-27 18:17:15 -0400

    minor updates in order to compile production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD9xx7CtzAi5IQ3wjdXy-v_wjM_ST3oYno",
+    authDomain: "footyverse-202523.firebaseapp.com",
+    databaseURL: "https://footyverse-202523.firebaseio.com",
+    projectId: "footyverse-202523",
+    storageBucket: "footyverse-202523.appspot.com",
+    messagingSenderId: "678639283287"
+  },
+  adClient: 'ca-pub-1686891852898803'
 };
